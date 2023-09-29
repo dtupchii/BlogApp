bc415883a1bf58a7026d5b5501f4b68e4006ad03
Daria Tupchii <dariatupchiy23@gmail.com>
2023-09-29 15:24:20 -0400

    Add project files.

---
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,41 @@
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AdminTagsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            Tag tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+            _db.Tags.Add(tag);
+            _db.SaveChanges();
+
+            return View("Add");
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            //_db.
+            return View();
+        }
+    }
+}
