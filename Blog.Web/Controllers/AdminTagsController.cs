e8467f6cd0e8d8d922ee70d597d81338cd5bb39f
Daria Tupchii <dariatupchiy23@gmail.com>
2023-10-13 11:44:01 -0400

    (Tags) Display All and Edit functionality added

--- Blog.Web/Controllers/AdminTagsController.cs
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -10,32 +10,91 @@ namespace Blog.Web.Controllers
         private readonly ApplicationDbContext _db;
         public AdminTagsController(ApplicationDbContext db)
         {
+            //dependency injection
             _db = db;
         }
 
         public IActionResult Add()
         {
+            //just returning view
             return View();
         }
         [HttpPost]
         public IActionResult Add(AddTagRequest addTagRequest)
         {
+            //creating a new tag object with values of VM object's properties (mapping)
             Tag tag = new Tag
             {
                 Name = addTagRequest.Name,
                 DisplayName = addTagRequest.DisplayName
             };
+
+            //adding new tag to DB
             _db.Tags.Add(tag);
             _db.SaveChanges();
 
-            return View("Add");
+            //going back to List View
+            return RedirectToAction("List");
         }
 
         [HttpGet]
         public IActionResult List()
         {
-            //_db.
-            return View();
+            //reading all Tags from DB
+            List<Tag> tagsFromDb = _db.Tags.ToList();
+
+            return View(tagsFromDb);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            //finding an object in DB by Id
+            var tagFromDb = _db.Tags.FirstOrDefault(a => a.Id == id);
+
+            if (tagFromDb != null) 
+            {
+                //mapping a VM object to show values on Edit View
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tagFromDb.Id,
+                    Name = tagFromDb.Name,
+                    DisplayName = tagFromDb.DisplayName
+                };
+                return View(editTagRequest);
+            }
+            else
+                return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest)
+        {
+            //"converting" VM object to Tag object
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+
+            //finding object in db by Id of tag object
+            var existingTag = _db.Tags.Find(tag.Id);
+
+            if (existingTag != null)
+            {
+                //updating values in db
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                //saving changes
+                _db.SaveChanges();
+
+                //going back to List View
+                return RedirectToAction("List");
+            }
+            else
+                return RedirectToAction("Edit", new {id = editTagRequest.Id});
         }
     }
 }
